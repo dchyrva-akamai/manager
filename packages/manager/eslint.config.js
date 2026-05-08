b03b6d439bdb5af175760477e347c8206204bdfd
Purvesh Makode <pmakode@akamai.com>
2026-05-08 08:26:36 +0000

    Pull request #189: cleanup: [UIE-11329]: Remove Cypress from Cloud Manager
    Merge in FEE/cloud-manager from feature/UIE-11329-cleanup-cypress to develop
    
    Squashed commit of the following:
    
    commit 9a6ee51c906439061a890aad03cc40564278f420
    Author: pmakode-akamai <pmakode@akamai.com>
    Date:   Fri May 8 13:06:27 2026 +0530
    
        Add changesets
    
    commit d222677c1371edb83270f2bffb476256648aab5d
    Author: pmakode-akamai <pmakode@akamai.com>
    Date:   Fri May 8 12:41:30 2026 +0530
    
        Updated pnpm-lock.yaml
    
    commit e6c99a95ef035ea1f69c2d4bcc710083ec06d1e4
    Merge: 0b29779e79 74ecb6ce83
    Author: pmakode-akamai <pmakode@akamai.com>
    Date:   Fri May 8 12:35:22 2026 +0530
    
        Resolve merge conflicts
    
    commit 0b29779e79500bc9efe6d19cbca8a3d58882dcf2
    Author: pmakode-akamai <pmakode@akamai.com>
    Date:   Fri May 8 12:21:29 2026 +0530
    
        Bump @testing-library/jest-dom to latest
    
    commit c60f4fe869084570a70272a2b3a782c6be4df4a4
    Author: pmakode-akamai <pmakode@akamai.com>
    Date:   Thu May 7 19:58:04 2026 +0530
    
        Remove cypress related packages and all its code references

--- packages/manager/eslint.config.js
+++ packages/manager/eslint.config.js
@@ -3,7 +3,6 @@ import eslint from '@eslint/js';
 import linodeRules from '@linode/eslint-plugin-cloud-manager/dist/index.js';
 import * as tsParser from '@typescript-eslint/parser';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import pluginCypress from 'eslint-plugin-cypress/flat';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
 import perfectionist from 'eslint-plugin-perfectionist';
 import prettier from 'eslint-plugin-prettier';
@@ -70,7 +69,6 @@ export const baseConfig = [
   js.configs.recommended,
   jsxA11y.flatConfigs.recommended,
   perfectionist.configs['recommended-natural'],
-  pluginCypress.configs.recommended,
   react.configs.flat.recommended,
   reactHooks.configs['recommended-latest'],
   sonarjs.configs.recommended,
@@ -100,13 +98,7 @@ export const baseConfig = [
         'error',
         {
           paths: restrictedImportPaths,
-          patterns: [
-            {
-              group: ['**/cypress/**'],
-              message:
-                'Cypress modules should only be imported in Cypress testing directories',
-            },
-          ],
+          patterns: [],
         },
       ],
       'no-restricted-syntax': [
@@ -128,13 +120,6 @@ export const baseConfig = [
       'spaced-comment': 'warn',
     },
   },
-  {
-    files: ['**/cypress.config.ts'],
-    rules: {
-      'no-restricted-imports': 'off',
-    },
-  },
-
   // 5. React, React Hooks, and React Refresh
   {
     files: ['**/*.{ts,tsx}'],
@@ -381,28 +366,7 @@ export const baseConfig = [
     },
   },
 
-  // 13. Cypress
-  {
-    files: ['**/cypress/**/*.{js,ts,tsx}'],
-    rules: {
-      'no-console': 'off',
-      'no-unused-expressions': 'off',
-      'sonarjs/pseudo-random': 'off',
-      'sonarjs/no-hardcoded-ip': 'off',
-      '@linode/cloud-manager/no-createLinode': 'error',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      // Maintain standard import restrictions but allow Cypress imports
-      'no-restricted-imports': [
-        'error',
-        {
-          paths: restrictedImportPaths,
-          // Intentionally omit patterns to allow Cypress imports here
-        },
-      ],
-    },
-  },
-
-  // 14. Prettier (coming last as recommended)
+  // 13. Prettier (coming last as recommended)
   {
     files: ['**/*.{js,ts,tsx}'],
     plugins: {
