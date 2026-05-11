8c00f4e5a52119545ff35107f4d75b96710aec5a
Karolina Gora <kagora@akamai.com>
2026-05-11 12:44:46 +0000

    Pull request #146: change: [DPS-42596] - Streams table - disable delete action
    Merge in FEE/cloud-manager from feature/DPS-42596-disable-delete-action to develop
    
    Squashed commit of the following:
    
    commit a2778b91286fa779ebd236cbcc1c791a05a02738
    Author: kagora <kagora@akamai.com>
    Date:   Wed Apr 29 12:15:58 2026 +0200
    
        change: [DPS-42596] - Streams table - disable delete action

--- packages/manager/src/features/Delivery/Streams/StreamActionMenu.test.tsx
+++ packages/manager/src/features/Delivery/Streams/StreamActionMenu.test.tsx
@@ -1,3 +1,4 @@
+import { streamStatus } from '@linode/api-v4';
 import { screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import * as React from 'react';
@@ -24,7 +25,7 @@ describe('StreamActionMenu', () => {
 
   describe('when stream is active', () => {
     it('should include proper Stream actions', async () => {
-      renderComponent('active');
+      renderComponent(streamStatus.Active);
 
       const actionMenuButton = screen.queryByLabelText(/^Action menu for/)!;
 
@@ -38,7 +39,7 @@ describe('StreamActionMenu', () => {
 
   describe('when stream is inactive', () => {
     it('should include proper Stream actions', async () => {
-      renderComponent('inactive');
+      renderComponent(streamStatus.Inactive);
 
       const actionMenuButton = screen.queryByLabelText(/^Action menu for/)!;
 
@@ -47,6 +48,68 @@ describe('StreamActionMenu', () => {
       for (const action of ['Edit', 'Activate', 'Delete']) {
         expect(screen.getByText(action)).toBeVisible();
       }
+    });
+  });
+
+  describe('when stream is provisioning', () => {
+    it('should include proper Stream actions, only Edit enabled', async () => {
+      renderComponent(streamStatus.Provisioning);
+
+      const actionMenuButton = screen.queryByLabelText(/^Action menu for/)!;
+
+      await userEvent.click(actionMenuButton);
+
+      for (const action of ['Activate', 'Delete']) {
+        expect(screen.getByRole('menuitem', { name: action })).toHaveAttribute(
+          'aria-disabled',
+          'true'
+        );
+      }
+
+      expect(
+        screen.getByRole('menuitem', { name: 'Edit' })
+      ).not.toHaveAttribute('aria-disabled');
+    });
+  });
+
+  describe('when stream is deactivating', () => {
+    it('should include proper Stream actions, only Edit enabled', async () => {
+      renderComponent(streamStatus.Deactivating);
+
+      const actionMenuButton = screen.queryByLabelText(/^Action menu for/)!;
+
+      await userEvent.click(actionMenuButton);
+
+      for (const action of ['Activate', 'Delete']) {
+        expect(screen.getByRole('menuitem', { name: action })).toHaveAttribute(
+          'aria-disabled',
+          'true'
+        );
+      }
+
+      expect(
+        screen.getByRole('menuitem', { name: 'Edit' })
+      ).not.toHaveAttribute('aria-disabled');
+    });
+  });
+
+  describe('when stream is failed', () => {
+    it('should include proper Stream actions, only Activate disabled', async () => {
+      renderComponent(streamStatus.Failed);
+
+      const actionMenuButton = screen.queryByLabelText(/^Action menu for/)!;
+
+      await userEvent.click(actionMenuButton);
+
+      for (const action of ['Delete', 'Edit']) {
+        expect(
+          screen.getByRole('menuitem', { name: action })
+        ).not.toHaveAttribute('aria-disabled');
+      }
+
+      expect(
+        screen.getByRole('menuitem', { name: 'Activate' })
+      ).toHaveAttribute('aria-disabled', 'true');
     });
   });
 });
