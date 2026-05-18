f935ed8ec31a6bf083f171987c61cf873eb1be90
Ganesh Revanakar <grevanak@akamai.com>
2026-05-18 05:19:52 +0000

    Pull request #201: fix: [UIE-10489] - Fix issue with reset of firewall selection in linode create and add interface forms
    Merge in FEE/cloud-manager from feature/UIE-10489-firewall-dropdown-issue to develop
    
    Squashed commit of the following:
    
    commit 53c0e92ae23701b8f2e2f1ea14e66a7950e58e18
    Author: Ganesh Revanakar <grevanak@akamai.com>
    Date:   Mon May 11 23:20:43 2026 +0530
    
        Fixed test failure
    
    commit baa2eb43ca333e31f96830f86da94b1ff1087af4
    Author: Ganesh Revanakar <grevanak@akamai.com>
    Date:   Mon May 11 22:18:53 2026 +0530
    
        Added changeset
    
    commit 2d0040c30858e2940d8fde91954e217b0c8a94dd
    Author: Ganesh Revanakar <grevanak@akamai.com>
    Date:   Mon May 11 20:26:28 2026 +0530
    
        fix: [UIE-10489] - Fix issue with reset of firewall selecion in linode create and add interface forms

--- packages/manager/src/features/Linodes/LinodeCreate/Networking/InterfaceType.tsx
+++ packages/manager/src/features/Linodes/LinodeCreate/Networking/InterfaceType.tsx
@@ -51,8 +51,11 @@ export const InterfaceType = ({ disabled, index }: Props) => {
 
   const { enqueueSnackbar } = useSnackbar();
 
-  const { control, getFieldState, setValue } =
+  const { control, getFieldState, getValues, setValue } =
     useFormContext<LinodeCreateFormValues>();
+  const firewallsByPurpose = React.useRef<
+    Partial<Record<InterfacePurpose, null | number | undefined>>
+  >({});
 
   const { field } = useController({
     control,
@@ -60,18 +63,37 @@ export const InterfaceType = ({ disabled, index }: Props) => {
   });
 
   const onChange = async (value: InterfacePurpose) => {
+    const firewallFieldName = `linodeInterfaces.${index}.firewall_id` as const;
+    const previousPurpose = field.value;
+    const currentFirewallId = getValues(firewallFieldName);
+
+    // Preserve firewall selections for non-VLAN purposes independently.
+    if (
+      previousPurpose &&
+      previousPurpose !== 'vlan' &&
+      currentFirewallId !== -1
+    ) {
+      firewallsByPurpose.current[previousPurpose] = currentFirewallId;
+    }
+
     // Change the interface purpose (Public, VPC, VLAN)
     field.onChange(value);
 
     // VLAN interfaces do not support Firewalls, so set
     // the Firewall ID to `-1` to be safe and early return.
     if (value === 'vlan') {
-      setValue(`linodeInterfaces.${index}.firewall_id`, -1);
+      setValue(firewallFieldName, -1);
+      return;
+    }
+
+    // Restore a previously selected firewall for this purpose, if available.
+    if (value in firewallsByPurpose.current) {
+      setValue(firewallFieldName, firewallsByPurpose.current[value]);
       return;
     }
 
     // If the user has not touched the Firewall field...
-    if (!getFieldState(`linodeInterfaces.${index}.firewall_id`).isTouched) {
+    if (!getFieldState(firewallFieldName).isTouched) {
       try {
         const firewallSettings = await queryClient.ensureQueryData(
           firewallQueries.settings
@@ -84,10 +106,9 @@ export const InterfaceType = ({ disabled, index }: Props) => {
 
         // If this Interface type has a default firewall, set it
         if (defaultFirewall) {
-          setValue(`linodeInterfaces.${index}.firewall_id`, defaultFirewall);
+          setValue(firewallFieldName, defaultFirewall);
         }
-        // eslint-disable-next-line sonarjs/no-ignored-exceptions
-      } catch (error) {
+      } catch {
         // The fetch to get Firewall Settings will fail for restricted users.
         enqueueSnackbar('Unable to retrieve default Firewall.', {
           variant: 'warning',
--- packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/LinodeInterfaces/AddInterfaceDrawer/AddInterfaceForm.test.tsx
+++ packages/manager/src/features/Linodes/LinodesDetail/LinodeNetworking/LinodeInterfaces/AddInterfaceDrawer/AddInterfaceForm.test.tsx
@@ -103,6 +103,39 @@ describe('AddInterfaceForm', () => {
     await findByDisplayValue(firewall.label);
   });
 
+  it('keeps selected Firewall when toggling VPC to VLAN and back to VPC', async () => {
+    const firewallSettings = firewallSettingsFactory.build({
+      default_firewall_ids: {
+        vpc_interface: 5,
+      },
+    });
+
+    const firewall = firewallFactory.build({ id: 5 });
+
+    server.use(
+      http.get('*/networking/firewalls/settings', () => {
+        return HttpResponse.json(firewallSettings);
+      }),
+      http.get('*/networking/firewalls', () => {
+        return HttpResponse.json(makeResourcePage([firewall]));
+      })
+    );
+
+    const { getByRole, findByDisplayValue, findByRole } = renderWithTheme(
+      <AddInterfaceForm {...props} />
+    );
+
+    await findByRole('radio', { name: 'VPC' });
+
+    await userEvent.click(getByRole('radio', { name: 'VPC' }));
+    await findByDisplayValue(firewall.label);
+
+    await userEvent.click(getByRole('radio', { name: 'VLAN' }));
+    await userEvent.click(getByRole('radio', { name: 'VPC' }));
+
+    await findByDisplayValue(firewall.label);
+  });
+
   it('should show a warning notice on selection of VPC option if a Public interface already exists', async () => {
     const mockPublicInterface = linodeInterfaceFactoryPublic.build();
 
